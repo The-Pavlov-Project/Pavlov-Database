bd6f00cdb9b9e82bfb676c3897c383c77880b5ad
not.simone <not.simone.scuola@outlook.com>
2020-02-10 18:19:28 +0100

    v0.0.6 (10 feb 2020) bug fix - action counter log fix (tuple immutable obj)

--- pvlv_database/common/action_counter_log.py
+++ pvlv_database/common/action_counter_log.py
@@ -58,14 +58,14 @@ class ActionCounterLog(object):
         # If the array is void, append the first item
         timestamp = timestamp.replace(minute=0, second=0, microsecond=0)
         if array_len is 0:
-            self.__log_by_hour.append((timestamp, counter, time_spent))
+            self.__log_by_hour.append([timestamp, counter, time_spent])
             return
 
         # If there are items in the array check the last one
         # if time delta has passed, insert a new item, else sum to the first one
         sub = timestamp - self.__log_by_hour[0][0]  # get time from first el
         if sub >= timedelta(hours=1):
-            self.__log_by_hour.insert(0, (timestamp, counter, time_spent))
+            self.__log_by_hour.insert(0, [timestamp, counter, time_spent])
         else:
             self.__log_by_hour[0][1] += counter
             self.__log_by_hour[0][2] += time_spent
@@ -85,14 +85,14 @@ class ActionCounterLog(object):
         # If the array is void, append the first item
         timestamp = timestamp.replace(hour=0, minute=0, second=0, microsecond=0)
         if array_len is 0:
-            self.__log_by_day.append((timestamp, counter, time_spent))
+            self.__log_by_day.append([timestamp, counter, time_spent])
             return
 
         # If there are items in the array check the last one
         # if time delta has passed, insert a new item, else sum to the first one
         sub = timestamp - self.__log_by_day[0][0]  # get time from first el
         if sub >= timedelta(days=1):
-            self.__log_by_day.insert(0, (timestamp, counter, time_spent))
+            self.__log_by_day.insert(0, [timestamp, counter, time_spent])
         else:
             self.__log_by_day[0][1] += counter
             self.__log_by_day[0][2] += time_spent
@@ -111,7 +111,7 @@ class ActionCounterLog(object):
 
         # If the array is void, append the first item
         if array_len is 0:
-            self.__log_by_month.append((timestamp, counter, time_spent))
+            self.__log_by_month.append([timestamp, counter, time_spent])
             return
 
         # If there are items in the array check the last one
@@ -119,7 +119,7 @@ class ActionCounterLog(object):
         db_timestamp = self.__log_by_month[0][0]  # get time from first el
 
         if timestamp.month > db_timestamp.month or (timestamp.month == 1 and db_timestamp.month == 12):
-            self.__log_by_month.insert(0, (timestamp, counter, time_spent))
+            self.__log_by_month.insert(0, [timestamp, counter, time_spent])
         else:
             self.__log_by_month[0][1] += counter
             self.__log_by_month[0][2] += time_spent
